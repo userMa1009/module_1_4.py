bb16bcbf40ededaf6c7471a9919eaabb1cc613f8
userMa1009 <kovalevamaria1009@gmail.com>
2024-10-22 13:15:23 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string=input("have a nice day")
+print("have a nice day",my_string)
+print(len(my_string))
+print('have a nice day'.upper())
+print("have a nice day".lower())
+#print('have a nice day'.replace(_old:' ',_new:'')) #не работает выдает ошибку, не понимаю почему(
+print(my_string[0])
+print(my_string[-1])
+print('havea nice day'[0])
+print('havea nice day'[-1])
+
